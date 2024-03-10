01bb7beba41a981094bb8b4a7dfcea5cf605ab0e
Namrata <namrata02.parekh@gmail.com>
2024-03-10 15:32:51 +0000

    new class added

---
+++ src/Test_Git.java
@@ -0,0 +1,13 @@
+
+    public class Test_Git {
+        static double w = 6;
+        static double l = 6;
+        static double a = w * l;
+
+        public static void main(String[] args) {
+
+            System.out.println(" the are of rectangle for the given value " + w + " and " + l + "+ is " +
+                    a + ".");
+        }
+
+    }
